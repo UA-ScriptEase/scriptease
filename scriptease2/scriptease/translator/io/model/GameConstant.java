abbd6f948c9de46bfbf6a5c9a199dbfb7c8dc6aa
Robin Miller <robinetmiller@gmail.com>
2012-07-26 15:33:53 -0600

    Fixed GameConstant.java disappearing (how did that happen?) and renamed a method in api dict.

---
+++ scriptease2/scriptease/translator/io/model/GameConstant.java
@@ -0,0 +1,33 @@
+package scriptease.translator.io.model;
+
+import scriptease.model.TypedComponent;
+
+public interface GameConstant extends TypedComponent {
+	/**
+	 * Gets the code that will resolve to an object as specified by the user.
+	 * 
+	 * @return The code string that represents the method of object resolution
+	 */
+	public String getResolutionText();
+
+	/**
+	 * The displayable name of the object
+	 * 
+	 * @return
+	 */
+	public String getName();
+
+	/**
+	 * Gets a tag for the object.
+	 * @return
+	 */
+	public String getTag();
+	
+	/**
+	 * Gets the template ID for the object. This is usually a stronger version
+	 * of a tag, although some games may not have both types. 
+	 * 
+	 * @return
+	 */
+	public String getTemplateID();
+}
