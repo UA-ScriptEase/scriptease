3f22e05dd1f52752dfd331c7202d25ad8162ba74
Robin Miller <robinetmiller@gmail.com>
2012-07-27 10:54:20 -0600

    Made it lose bindings when they are not found, rather than crashing.

--- scriptease2/scriptease/controller/io/converter/KnowItBindingConverter.java
+++ scriptease2/scriptease/controller/io/converter/KnowItBindingConverter.java
@@ -247,7 +247,7 @@ public class KnowItBindingConverter implements Converter {
 		// Let's figure out which subtype of KnowItBinding we want.
 		if (flavour == null
 				|| flavour.equalsIgnoreCase(ATTRIBUTE_VALUE_NULL_FLAVOUR))
-			binding = new KnowItBindingNull();
+			binding = null;
 		else {
 			reader.getNodeName();
 
@@ -275,7 +275,10 @@ public class KnowItBindingConverter implements Converter {
 						+ flavour);
 		}
 
-		return binding;
+		if (binding == null)
+			return new KnowItBindingNull();
+		else
+			return binding;
 	}
 
 	private KnowItBindingConstant unmarshallConstantBinding(
@@ -355,14 +358,14 @@ public class KnowItBindingConverter implements Converter {
 			HierarchicalStreamReader reader, UnmarshallingContext context) {
 		final KnowIt referent;
 		KnowItBindingReference binding = new KnowItBindingReference(null);
-		
+
 		// move down and read as a knowIt
 		reader.moveDown();
 		referent = (KnowIt) context.convertAnother(binding, KnowIt.class);
 		reader.moveUp();
-		
+
 		binding = new KnowItBindingReference(referent);
-		
+
 		return binding;
 	}
 
