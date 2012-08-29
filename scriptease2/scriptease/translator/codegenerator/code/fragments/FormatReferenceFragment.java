10977e6cdd9b98bc1c4af9ba023d9b5993133e29
Kevin Schenk <kevin@kschenk.com>
2012-08-29 15:04:44 -0600

    Saving stories works again.

--- scriptease2/scriptease/translator/codegenerator/code/fragments/FormatReferenceFragment.java
+++ scriptease2/scriptease/translator/codegenerator/code/fragments/FormatReferenceFragment.java
@@ -1,7 +1,11 @@
 package scriptease.translator.codegenerator.code.fragments;
 
 import scriptease.translator.codegenerator.CharacterRange;
+import scriptease.translator.codegenerator.CodeGenerationKeywordConstants.FormatReferenceType;
+import scriptease.translator.codegenerator.code.contexts.AskItContext;
 import scriptease.translator.codegenerator.code.contexts.Context;
+import scriptease.translator.codegenerator.code.contexts.KnowItContext;
+import scriptease.translator.codegenerator.code.contexts.ScriptItContext;
 
 /**
  * Represents a code location where another format from the format dictionary is
@@ -10,6 +14,12 @@ import scriptease.translator.codegenerator.code.contexts.Context;
  * @author remiller
  */
 public class FormatReferenceFragment extends AbstractFragment {
+	private final FormatReferenceType type;
+
+	public FormatReferenceFragment(String text) {
+		this(text, FormatReferenceType.NONE);
+	}
+
 	/**
 	 * See:
 	 * {@link AbstractFragment#FormatFragment(String, CharacterRange, char[])}
@@ -17,8 +27,13 @@ public class FormatReferenceFragment extends AbstractFragment {
 	 * @param text
 	 *            The format reference label.
 	 */
-	public FormatReferenceFragment(String text) {
+	public FormatReferenceFragment(String text, FormatReferenceType type) {
 		super(text);
+		this.type = type;
+	}
+	
+	public FormatReferenceType getType() {
+		return this.type;
 	}
 
 	/**
@@ -28,9 +43,16 @@ public class FormatReferenceFragment extends AbstractFragment {
 	@Override
 	public String resolve(Context context) {
 		final String format = this.getDirectiveText();
-		return AbstractFragment.resolveFormat(context.getTranslator()
-				.getLanguageDictionary().getFormat(format.toUpperCase()),
-				context);
+
+		if ((this.type == FormatReferenceType.NONE)
+				|| (this.type == FormatReferenceType.ASKIT && context instanceof AskItContext)
+				|| (this.type == FormatReferenceType.SCRIPTIT && context instanceof ScriptItContext)
+				|| (this.type == FormatReferenceType.KNOWIT && context instanceof KnowItContext))
+			return AbstractFragment.resolveFormat(context.getTranslator()
+					.getLanguageDictionary().getFormat(format.toUpperCase()),
+					context);
+		else
+			return "";
 	}
 
 	@Override
