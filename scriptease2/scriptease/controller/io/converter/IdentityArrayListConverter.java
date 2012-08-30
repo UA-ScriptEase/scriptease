83fabef949d1f02b275e9609d9d9b54e88b9e1ef
Abbie Schenk <aschenk@ualberta.ca>
2012-08-30 16:01:42 -0600

    Commented IdentityArrayListConverter

--- scriptease2/scriptease/controller/io/converter/IdentityArrayListConverter.java
+++ scriptease2/scriptease/controller/io/converter/IdentityArrayListConverter.java
@@ -5,12 +5,19 @@ import sun.awt.util.IdentityArrayList;
 import com.thoughtworks.xstream.converters.collections.CollectionConverter;
 import com.thoughtworks.xstream.mapper.Mapper;
 
-public class IdentityArrayListConverter extends CollectionConverter{
+/**
+ * The reason we need this class is because the regular ol' CollectionConverter
+ * can't deal with IdentityArrayLists.
+ * 
+ * @author kschenk
+ * 
+ */
+public class IdentityArrayListConverter extends CollectionConverter {
 
 	public IdentityArrayListConverter(Mapper mapper) {
 		super(mapper);
 	}
-	
+
 	@SuppressWarnings("rawtypes")
 	@Override
 	public boolean canConvert(Class type) {
