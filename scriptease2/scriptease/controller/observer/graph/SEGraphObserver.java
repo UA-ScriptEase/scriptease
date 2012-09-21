6f64fdae2227ea8c165d38e9f04afd1b66699930
Kevin Schenk <kevin@kschenk.com>
2012-09-21 12:10:15 -0600

    Added SEGraphObserver and Adapter to commit. Previous commit will not work without these.

---
+++ scriptease2/scriptease/controller/observer/graph/SEGraphObserver.java
@@ -0,0 +1,63 @@
+package scriptease.controller.observer.graph;
+
+import java.util.Collection;
+
+/**
+ * Allows implementers to be notified of changes to the graph. Use
+ * {@link SEGraphAdapter} to only override certain methods.
+ * 
+ * @author kschenk
+ * 
+ */
+public interface SEGraphObserver {
+
+	/**
+	 * Called when two nodes have been connected.
+	 * 
+	 * @param child
+	 *            The new child
+	 * @param parent
+	 *            The new parent
+	 */
+	public void nodesConnected(Object child, Object parent);
+
+	/**
+	 * Called when two nodes have been disconnected.
+	 * 
+	 * @param child
+	 *            The old child
+	 * @param parent
+	 *            The old parent
+	 */
+	public void nodesDisconnected(Object child, Object parent);
+
+	/**
+	 * Called when a node has been added to the graph.
+	 * 
+	 * @param newNode
+	 *            The new node added to the graph
+	 * @param children
+	 *            The children of the new node
+	 * @param parents
+	 *            The parents of the new node
+	 */
+	public void nodeAdded(Object newNode, Collection<?> children,
+			Collection<?> parents);
+
+	/**
+	 * Called when a node has been removed from the graph.
+	 * 
+	 * @param removedNode
+	 *            The node that was removed from the graph.
+	 */
+	public void nodeRemoved(Object removedNode);
+
+	/**
+	 * Called when a node has been selected.
+	 * 
+	 * @param node
+	 *            The selected node.
+	 */
+	public void nodeSelected(Object node);
+
+}
