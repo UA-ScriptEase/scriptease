965afb46ef0e475e58b0e99189192352420e3ba4
Abbie Schenk <aschenk@ualberta.ca>
2012-09-25 09:55:47 -0600

    Game Object Panel works like normal again plus indented conversations

--- scriptease2/scriptease/gui/SETree/GameObjectPanelTree.java
+++ scriptease2/scriptease/gui/SETree/GameObjectPanelTree.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -46,8 +47,6 @@ public class GameObjectPanelTree extends JPanel {
 
 	Filter filter;
 
-	private final int CONSTANT_OFFSET = 10;
-
 	public GameObjectPanelTree() {
 		super();
 		this.treeModel = new Tree<Object>("Available Game Objects");
@@ -64,13 +63,15 @@ public class GameObjectPanelTree extends JPanel {
 	}
 
 	/**
-	 * Filter the StoryComponentPanelTree immediate children, does nothing if no
+	 * Filter the StoryComponentPanelTree immediate children. Does nothing if no
 	 * filter is applied
 	 * 
 	 * @return
 	 */
 	private List<GameConstant> filterGameObjects(List<GameConstant> gameObjects) {
-		List<GameConstant> filteredObjects = new ArrayList<GameConstant>();
+		final List<GameConstant> filteredObjects;
+
+		filteredObjects = new ArrayList<GameConstant>();
 
 		if (this.filter == null)
 			if (gameObjects == null)
@@ -89,6 +90,12 @@ public class GameObjectPanelTree extends JPanel {
 		return filteredObjects;
 	}
 
+	/**
+	 * Returns all objects of the specified type.
+	 * 
+	 * @param type
+	 * @return
+	 */
 	private Collection<GameConstant> getObjectsOfType(final String type) {
 		final PatternModel activeModel;
 		final List<GameConstant> allGameObjects = new ArrayList<GameConstant>();
@@ -116,6 +123,9 @@ public class GameObjectPanelTree extends JPanel {
 		return allGameObjects;
 	}
 
+	/**
+	 * Draws the tree.
+	 */
 	public void drawTree() {
 		this.removeAll();
 		this.treeModel.clear();
@@ -215,38 +225,42 @@ public class GameObjectPanelTree extends JPanel {
 	}
 
 	/**
-	 * Container of Game Objects.
+	 * Container of Game Objects. Displays the game objects inside it, and can
+	 * be collapsed or expanded.
 	 * 
 	 * @author kschenk
 	 * 
 	 */
 	private class GameObjectContainer extends JPanel {
-		private final String labelName;
+		private final String label;
 
 		private boolean collapsed;
 
+		/**
+		 * Creates a new game object container with the passed in name.
+		 * 
+		 * @param label
+		 */
 		public GameObjectContainer(String label) {
 			this.collapsed = false;
-			this.labelName = label;
+			this.label = label;
 
 			this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 			this.setBackground(Color.WHITE);
 
-			redrawLabel();
+			redrawContainer();
 		}
 
-		private String convertToLabelViewFormat() {
-			String labelView = this.labelName.substring(0, 1).toUpperCase();
-			labelView += this.labelName.substring(1);
-			return labelView;
-		}
-
-		private void redrawLabel() {
+		/**
+		 * Redraws the container. This also fires whenever the container is
+		 * collapsed or expanded.
+		 */
+		private void redrawContainer() {
 			this.removeAll();
 
 			final JLabel categoryLabel;
 
-			categoryLabel = new JLabel(convertToLabelViewFormat());
+			categoryLabel = new JLabel(StringOp.toProperCase(this.label));
 
 			categoryLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
 
@@ -254,7 +268,7 @@ public class GameObjectPanelTree extends JPanel {
 				@Override
 				public void mouseClicked(MouseEvent e) {
 					GameObjectContainer.this.collapsed = !GameObjectContainer.this.collapsed;
-					redrawLabel();
+					redrawContainer();
 				}
 			});
 
@@ -268,13 +282,13 @@ public class GameObjectPanelTree extends JPanel {
 				final Collection<Object> successors;
 
 				successors = GameObjectPanelTree.this.treeModel
-						.getSuccessors(labelName);
+						.getSuccessors(label);
 
 				for (Object successor : successors) {
 					if (successor instanceof GameObject)
-						this.add(createGameObjectComponent((GameObject) successor));
+						this.add(createGameConstantPanel((GameObject) successor));
 					else if (successor instanceof GameConversation)
-						this.add(createGameConversationComponent((GameConversation) successor));
+						this.add(createGameConversationPanel((GameConversation) successor));
 				}
 			}
 
@@ -283,7 +297,15 @@ public class GameObjectPanelTree extends JPanel {
 			this.revalidate();
 		}
 
-		private JPanel createGameConversationComponent(
+		/**
+		 * Creates a game conversation panel using the passed in game
+		 * conversation. This adds all of the game conversation's successors to
+		 * the list as well.
+		 * 
+		 * @param gameConversation
+		 * @return
+		 */
+		private JPanel createGameConversationPanel(
 				GameConversation gameConversation) {
 			final JPanel convoPanel;
 
@@ -293,14 +315,21 @@ public class GameObjectPanelTree extends JPanel {
 			convoPanel
 					.setLayout(new BoxLayout(convoPanel, BoxLayout.PAGE_AXIS));
 
-			convoPanel.add(createGameObjectComponent(gameConversation));
+			convoPanel.add(createGameConstantPanel(gameConversation));
 
 			for (Object gameConversationNode : GameObjectPanelTree.this.treeModel
 					.getSuccessors(gameConversation)) {
-				convoPanel
-						.add(createGameObjectComponent((GameConversationNode) gameConversationNode));
+
+				int indent;
+
+				indent = 1;
+
+				convoPanel.add(createIndentedPanel(
+						(GameConversationNode) gameConversationNode, indent));
+
 				this.addConversationRoots(
-						(GameConversationNode) gameConversationNode, convoPanel);
+						(GameConversationNode) gameConversationNode,
+						convoPanel, indent + 1);
 			}
 
 			convoPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
@@ -308,7 +337,15 @@ public class GameObjectPanelTree extends JPanel {
 			return convoPanel;
 		}
 
-		private JPanel createGameObjectComponent(GameConstant gameObject) {
+		/**
+		 * Creates a JPanel for the passed in GameConstant. This panel contains
+		 * a binding widget that the user can then drag and drop into the
+		 * appropriate slots.
+		 * 
+		 * @param gameObject
+		 * @return
+		 */
+		private JPanel createGameConstantPanel(GameConstant gameObject) {
 			final JPanel objectPanel;
 			final BindingWidget gameObjectBindingWidget;
 			final String regularText;
@@ -328,10 +365,43 @@ public class GameObjectPanelTree extends JPanel {
 			gameObjectBindingWidget = new BindingWidget(
 					new KnowItBindingConstant(gameObject));
 
-			if (StringOp.wordCount(regularText) > 5)
+			// If the name is longer than 5 characters, shorten it and add a
+			// MouseListener to display longer name when clicked
+			if (StringOp.wordCount(regularText) > 5) {
+				final String shortText;
+
+				shortText = shortenText(regularText);
+
 				gameObjectBindingWidget.add(ScriptWidgetFactory.buildLabel(
-						createShortHandViewofText(regularText), Color.WHITE));
-			else
+						shortText, Color.WHITE));
+
+				gameObjectBindingWidget.addMouseListener(new MouseAdapter() {
+					boolean textIsShortened = true;
+
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						final String newText;
+
+						for (Component component : gameObjectBindingWidget
+								.getComponents())
+							if (component instanceof JLabel)
+								gameObjectBindingWidget.remove(component);
+
+						if (textIsShortened)
+							newText = regularText;
+						else
+							newText = shortText;
+
+						gameObjectBindingWidget.add(ScriptWidgetFactory
+								.buildLabel(newText, Color.WHITE));
+
+						gameObjectBindingWidget.revalidate();
+
+						textIsShortened = !textIsShortened;
+					}
+				});
+			} else
+				// Otherwise, just use the normal, long name
 				gameObjectBindingWidget.add(ScriptWidgetFactory.buildLabel(
 						regularText, Color.WHITE));
 
@@ -349,17 +419,22 @@ public class GameObjectPanelTree extends JPanel {
 		}
 
 		/**
-		 * Recursively add all children roots to tree.
+		 * Recursively adds all successors of the passed in GameConversationNode
+		 * to the passed in JPanel.
 		 * 
 		 * @param parent
+		 * @param convoPanel
+		 * @param indent
 		 */
 		private void addConversationRoots(GameConversationNode parent,
-				JPanel convoPanel) {
+				JPanel convoPanel, int indent) {
 			if (parent.isTerminal()) {
 				return;
 			}
 
-			Collection<Object> successors = GameObjectPanelTree.this.treeModel
+			final Collection<Object> successors;
+
+			successors = GameObjectPanelTree.this.treeModel
 					.getSuccessors(parent);
 
 			if (successors.size() == 0) {
@@ -367,16 +442,55 @@ public class GameObjectPanelTree extends JPanel {
 			}
 
 			for (Object successor : successors) {
-				if (successor instanceof GameConversation)
-					convoPanel
-							.add(createGameObjectComponent((GameConversation) successor));
-				else if (successor instanceof GameConversationNode)
+				if (successor instanceof GameConversation) {
+					convoPanel.add(createIndentedPanel(
+							(GameConversation) successor, indent));
+				} else if (successor instanceof GameConversationNode) {
+					convoPanel.add(createIndentedPanel(
+							(GameConversationNode) successor, indent));
 					this.addConversationRoots((GameConversationNode) successor,
-							convoPanel);
+							convoPanel, indent + 1);
+
+				}
 			}
 		}
 
-		private String createShortHandViewofText(final String text) {
+		/**
+		 * Creates an indented panel for the passed in GameConstant using the
+		 * indent. Indents are made using a
+		 * {@link Box#createRigidArea(Dimension)}, where the dimension is
+		 * (5*indent, 0).
+		 * 
+		 * @param gameConstant
+		 * @param indent
+		 * @return
+		 */
+		private JPanel createIndentedPanel(GameConstant gameConstant, int indent) {
+			final int STRUT_SIZE = 10 * indent;
+			final JPanel indentedPanel;
+
+			indentedPanel = new JPanel();
+
+			indentedPanel.setLayout(new BoxLayout(indentedPanel,
+					BoxLayout.LINE_AXIS));
+
+			indentedPanel.setOpaque(false);
+
+			indentedPanel.add(Box.createHorizontalStrut(STRUT_SIZE));
+			indentedPanel.add(createGameConstantPanel(gameConstant));
+
+			indentedPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+			return indentedPanel;
+		}
+
+		/**
+		 * Shortens the text to fit on the binding widget.
+		 * 
+		 * @param text
+		 * @return
+		 */
+		private String shortenText(final String text) {
 			String truncatedVersion = "";
 			int firstSpace = 0;
 			int secondSpace = 0;
