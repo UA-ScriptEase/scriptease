a388d5c83a98c707eab9fc3d19dfb3b10def3518
Kevin Schenk <kevin@kschenk.com>
2013-04-25 12:22:50 -0600

    Added an action to open library editors

--- scriptease2/scriptease/gui/action/library/OpenAPIDictionaryEditorAction.java
+++ scriptease2/scriptease/gui/action/library/OpenAPIDictionaryEditorAction.java
@@ -4,24 +4,27 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 
-import scriptease.gui.internationalization.Il8nResources;
 import scriptease.model.SEModelManager;
 import scriptease.translator.Translator;
 import scriptease.translator.TranslatorManager;
 
+/**
+ * Opens the editor for the API Dictionary.
+ * 
+ * @author kschenk
+ * 
+ */
 @SuppressWarnings("serial")
-public class OpenLibraryEditorAction extends AbstractAction {
-	private static final String OPEN_MODEL = Il8nResources
-			.getString("Open_Model");
-
+public class OpenAPIDictionaryEditorAction extends AbstractAction {
 	private final Translator translator;
 
 	/**
-	 * Defines an <code>OpenModelAction</code> object with accelerator and
-	 * mnemonic.
+	 * Creates an open editor action for the translator's api dictionary.
+	 * 
+	 * @param translator
 	 */
-	public OpenLibraryEditorAction(Translator translator) {
-		super(OpenLibraryEditorAction.OPEN_MODEL + "...");
+	public OpenAPIDictionaryEditorAction(Translator translator) {
+		super(translator.getName());
 
 		this.translator = translator;
 	}
