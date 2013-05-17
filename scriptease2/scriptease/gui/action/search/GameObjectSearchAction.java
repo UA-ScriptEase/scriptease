f76d2041788d5e54dd08e8e089d3cfd364de1af4
Jessica Yuen <jyuen@ualberta.ca>
2013-05-17 16:52:40 -0600

    Added GameObjectSearchPanel and the retrieval of all game objects

--- scriptease2/scriptease/gui/action/search/GameObjectSearchAction.java
+++ scriptease2/scriptease/gui/action/search/GameObjectSearchAction.java
@@ -2,20 +2,37 @@ package scriptease.gui.action.search;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.Action;
 import javax.swing.KeyStroke;
 
-import scriptease.controller.MetricsAnalyzer;
+import scriptease.controller.BindingAdapter;
+import scriptease.controller.StoryAdapter;
 import scriptease.gui.WindowFactory;
 import scriptease.gui.action.ActiveModelSensitiveAction;
+import scriptease.gui.pane.GameObjectSearchPanel;
+import scriptease.model.SEModelManager;
+import scriptease.model.StoryComponent;
+import scriptease.model.atomic.KnowIt;
+import scriptease.model.atomic.knowitbindings.KnowItBindingReference;
+import scriptease.model.atomic.knowitbindings.KnowItBindingResource;
+import scriptease.model.complex.CauseIt;
+import scriptease.model.complex.ComplexStoryComponent;
+import scriptease.model.complex.ScriptIt;
+import scriptease.model.complex.StoryComponentContainer;
+import scriptease.model.complex.StoryPoint;
+import scriptease.translator.TranslatorManager;
+import scriptease.translator.apimanagers.DescribeItManager;
 
 /**
- * Listens for prompt to open a search (and optional replace) window for game
- * objects in the current active story model.
+ * Listens for prompt to open a global search (and optional replace) window for
+ * game objects in the current active story model.
  * 
  * @author jyuen
  */
+@SuppressWarnings("serial")
 public class GameObjectSearchAction extends ActiveModelSensitiveAction {
 	private static final String GAME_OBJECT_SEARCH = "Search for Game Objects";
 
@@ -49,17 +66,84 @@ public class GameObjectSearchAction extends ActiveModelSensitiveAction {
 
 	@Override
 	/**
-	 * Create the metrics dialog and call Metric Analyzer to process the story
-	 * components so we can make data out of it!
+	 * Create the search and replace frame and process the components in the 
+	 * current story point so we can retrieve our game objects!
 	 */
 	public void actionPerformed(ActionEvent arg0) {
-		MetricsAnalyzer.getInstance().processStoryComponents();
-
 		createSearchFrame();
 	}
 
 	private void createSearchFrame() {
 		WindowFactory.getInstance().buildAndShowCustomFrame(
-				new GameObjectSearchPanel(), "Find/Replace Game Objects", true);
+				new GameObjectSearchPanel(this.getExistingGameObjects()),
+				"Find/Replace Game Objects", true);
+	}
+
+	private List<KnowIt> getExistingGameObjects() {
+		final StoryPoint root = SEModelManager.getInstance().getActiveRoot();
+		final StoryAdapter adapter;
+
+		final List<KnowIt> gameObjects = new ArrayList<KnowIt>();
+
+		adapter = new StoryAdapter() {
+			@Override
+			public void processStoryPoint(StoryPoint storyPoint) {
+				this.defaultProcessComplex(storyPoint);
+				for (StoryPoint successor : storyPoint.getSuccessors())
+					successor.process(this);
+			}
+
+			@Override
+			protected void defaultProcessComplex(ComplexStoryComponent complex) {
+				for (StoryComponent child : complex.getChildren()) {
+					child.process(this);
+				}
+			}
+
+			@Override
+			public void processCauseIt(CauseIt causeIt) {
+				causeIt.processParameters(this);
+				this.defaultProcessComplex(causeIt);
+			}
+
+			@Override
+			public void processScriptIt(ScriptIt scriptIt) {
+				scriptIt.processParameters(this);
+				this.defaultProcessComplex(scriptIt);
+			}
+
+			@Override
+			public void processKnowIt(final KnowIt knowIt) {
+				DescribeItManager describeItManager = TranslatorManager
+						.getInstance().getActiveDescribeItManager();
+
+				if (describeItManager.getDescribeIt(knowIt) == null) {
+
+					knowIt.getBinding().process(new BindingAdapter() {
+
+						@Override
+						public void processReference(
+								KnowItBindingReference reference) {
+							reference.getValue().getBinding().process(this);
+						}
+
+						public void processConstant(
+								KnowItBindingResource resource) {
+							if (resource.isIdentifiableGameConstant())
+								gameObjects.add(knowIt);
+						};
+					});
+				}
+			}
+
+			@Override
+			public void processStoryComponentContainer(
+					StoryComponentContainer container) {
+				this.defaultProcessComplex(container);
+			}
+		};
+
+		root.process(adapter);
+		return gameObjects;
 	}
 }
