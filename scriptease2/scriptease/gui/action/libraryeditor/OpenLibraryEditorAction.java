5f232caa1bd9e69f1f8d2d78ad58f509233aa408
Kevin Schenk <kevin@kschenk.com>
2013-05-28 13:51:11 -0600

    OpenLib Editor ACtion wasnt in git.....

---
+++ scriptease2/scriptease/gui/action/libraryeditor/OpenLibraryEditorAction.java
@@ -0,0 +1,36 @@
+package scriptease.gui.action.libraryeditor;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import scriptease.gui.WindowFactory;
+import scriptease.translator.Translator;
+
+/**
+ * Opens the a dialog that allows the user to choose a Library to edit.
+ * 
+ * @author kschenk
+ * 
+ */
+@SuppressWarnings("serial")
+public class OpenLibraryEditorAction extends AbstractAction {
+	private final Translator translator;
+
+	/**
+	 * Creates an open editor action for the translator's library.
+	 * 
+	 * @param translator
+	 */
+	public OpenLibraryEditorAction(Translator translator) {
+		super("Edit " + translator.getName() + " Libraries");
+		this.translator = translator;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		WindowFactory.getInstance()
+				.buildLibraryEditorChoiceDialog(this.translator)
+				.setVisible(true);
+	}
+}
