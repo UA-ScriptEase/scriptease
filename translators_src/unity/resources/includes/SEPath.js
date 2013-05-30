57e74d4acc25ea322993396021538285ddaf7506
Kevin Schenk <kevin@kschenk.com>
2013-05-30 13:54:28 -0600

    Pathfinding should theoretically work now

---
+++ translators_src/unity/resources/includes/SEPath.js
@@ -0,0 +1,53 @@
+#pragma strict
+
+/**
+ * This class defines an SEPath. It has some shortcuts that make the ScriptEase
+ * II implementation much easier and cleaner. 
+ *
+ * The SEPath is part of the optional A* Pathfinding Library, which requires
+ * Aron Granberg's A* Pathfinding Project package to be installed to your Unity
+ * project. Our library was built and functions with the free version.
+ *
+ *@author ScriptEase II Team
+ */
+
+/**
+ * Returns the SEPath attached to the object. Attaches an SEPath if none are
+ * attached. This allows the experienced game developer to add an SEPath in the
+ * editor but still make use of ScriptEase II.
+ */
+static function GetPath(object:GameObject):SEPath {
+	var sePath:SEPath = object.GetComponent(SEPath);
+		
+	if(sePath == null) {
+		object.AddComponent(SEPath);
+		sePath = object.GetComponent(SEPath);
+	}
+	
+	return sePath;
+}
+
+/**
+ * We extend the AIPath class so that we can add our custom function to it,
+ * which is set in a Cause.
+ *
+ */
+class SEPath extends AIPath {
+	
+	var onReached:Function;
+	
+	/**
+	 * Sets what happens when the target is reached
+	 */
+	function setOnTargetReached(onReached:Function) {
+		this.onReached = onReached;
+	}
+
+	/**
+	 * Overidden function from AIPath, since it by default does not have any
+	 * functionality. All this does is call the onReached function variable.
+	 */
+	function OnTargetReached () {
+		onReached();	
+	}
+}
