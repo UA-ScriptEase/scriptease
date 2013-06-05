5bd6b13093392a73b21e4f018889b69a732b3aa0
Jessica Yuen <jyuen@ualberta.ca>
2013-06-05 14:38:16 -0600

    Added ControlIt for GUI groups

--- scriptease2/scriptease/model/complex/ControlIt.java
+++ scriptease2/scriptease/model/complex/ControlIt.java
@@ -30,7 +30,7 @@ public class ControlIt extends ScriptIt {
 	 * 
 	 */
 	public enum ControlItFormat {
-		NONE, DELAY, REPEAT, QUESTION
+		NONE, DELAY, REPEAT, QUESTION, FOLDER
 	}
 
 	private ControlItFormat format;
