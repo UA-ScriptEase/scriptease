9da87d357fcebe323734de8a59734e3ecc788c30
Jessica Yuen <jyuen@ualberta.ca>
2013-06-06 15:58:07 -0600

    Added more changes to the defaultUnityLibrary

--- scriptease2/scriptease/model/complex/ControlIt.java
+++ scriptease2/scriptease/model/complex/ControlIt.java
@@ -55,6 +55,7 @@ public class ControlIt extends ScriptIt {
 		this.registerChildType(StoryComponentContainer.class, max);
 		this.registerChildType(Note.class, max);
 		this.registerChildType(ControlIt.class, max);
+		this.registerChildType(AskIt.class, max);
 	}
 
 	/**
