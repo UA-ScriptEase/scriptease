d81efbae990909a5a29536f9e3e81f3f255e4d0b
Jessica Yuen <jyuen@ualberta.ca>
2013-09-19 10:48:05 -0600

    Added some metric related classes

---
+++ scriptease2/scriptease/model/semodel/ScriptEaseKeywords.java
@@ -0,0 +1,51 @@
+package scriptease.model.semodel;
+
+/**
+ * Common interface for all of the ScriptEase keywords.
+ * 
+ * @author jyuen
+ */
+public interface ScriptEaseKeywords {
+
+	public static final String STORY_COMPONENTS = "Story Components";
+
+	public static final String STORY_COMPONENT = "Story Component";
+
+	public static final String STORY_POINT = "Story Point";
+
+	public static final String COMPLEXITY = "Complexity";
+
+	public static final String COMPONENT_COUNT = "Component Count";
+
+	public static final String FAVOURITE = "Favourite";
+
+	public static final String FREQUENCY = "Frequency";
+
+	public static final String AVERAGE = "Average";
+
+	public static final String CAUSE = "Cause";
+	
+	public static final String CAUSES = "Causes";
+	
+	public static final String EFFECT = "Effect";
+	
+	public static final String EFFECTS = "Effects";
+
+	public static final String DESCRIPTIONS = "Descriptions";
+
+	public static final String QUESTIONS = "Questions";
+
+	public static final String REPEATS = "Repeats";
+
+	public static final String DELAYS = "Delays";
+	
+	public static final String STORY_POINTS = "Story Points";
+	
+	public static final String GAME_OBJECTS = "Game Objects";
+	
+	public static final String IMPLICITS = "Implicits";
+	
+	public static final String NOTES = "Notes";
+
+	public static final String STORY_POINT_COMPLEXITY = "Story Point Complexity";
+}
