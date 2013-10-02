f822b9f935322c104db7b23d732214be07833a52
Jessica Yuen <jyuen@ualberta.ca>
2013-10-02 14:10:33 -0600

    Added prototype behaviour classes

---
+++ scriptease2/scriptease/model/complex/behaviours/IndependentBehaviour.java
@@ -0,0 +1,23 @@
+package scriptease.model.complex.behaviours;
+
+import scriptease.controller.StoryVisitor;
+
+public class IndependentBehaviour extends Behaviour {
+
+	public IndependentBehaviour(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void process(StoryVisitor visitor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void revalidateKnowItBindings() {
+		// TODO Auto-generated method stub
+		
+	}
+}
